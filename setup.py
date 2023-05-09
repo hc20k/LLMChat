5bfdf979132be1a08d7bbf0577043c23854a362d
pluto <harrison.coutee@gmail.com>
2023-05-09 11:42:33 -0700

    Instead of using parent dir, symlink example config before build

--- setup.py
+++ setup.py
@@ -1,2 +1,25 @@
 from setuptools import setup
-setup()
+import os
+from setuptools.command.install import install
+from setuptools.command.build_py import build_py
+
+class PreInstall(install):
+    def run(self):
+        os.symlink("config.example.ini", "llmchat/config.example.ini")
+        install.run(self)
+        os.unlink("llmchat/config.example.ini")
+
+
+class PreBuild(build_py):
+    def run(self):
+        os.symlink("config.example.ini", "llmchat/config.example.ini")
+        build_py.run(self)
+        os.unlink("llmchat/config.example.ini")
+
+
+setup(
+    cmdclass={
+        'install': PreInstall,
+        'build_py': PreBuild
+    }
+)
