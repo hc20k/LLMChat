9654d6b6ca6a92e4c54727fd4b62c17cc75b27aa
pluto <harrison.coutee@gmail.com>
2023-05-09 11:52:09 -0700

    Sigh... use copy instead

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
+import shutil
 import os
 from setuptools.command.install import install
 from setuptools.command.build_py import build_py
 
 class PreInstall(install):
     def run(self):
-        os.symlink("config.example.ini", "llmchat/config.example.ini")
+        shutil.copy("config.example.ini", "llmchat/config.example.ini")
         install.run(self)
         os.unlink("llmchat/config.example.ini")
 
 
 class PreBuild(build_py):
     def run(self):
-        os.symlink("config.example.ini", "llmchat/config.example.ini")
+        shutil.copy("config.example.ini", "llmchat/config.example.ini")
         build_py.run(self)
         os.unlink("llmchat/config.example.ini")
 
